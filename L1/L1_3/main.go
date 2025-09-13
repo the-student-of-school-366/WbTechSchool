e1de0d5611e3c7da36d55382dc1c97a9f684b76b
thestudentofschool@gmail.com <ananda.uldanov.2005@gmail.com>
2025-09-13 20:11:12 +0300

    Добавлены комментарии

--- L1/L1_3/main.go
+++ L1/L1_3/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// worker принимает из канала ввода число, выводит его в консоль, имитирует работу, и вывод сообщение в канал вывода
 func worker(id int, jobs <-chan int, results chan<- string) {
 	for j := range jobs {
 		fmt.Println("worker", id, "выел:", j)
@@ -15,23 +16,29 @@ func worker(id int, jobs <-chan int, results chan<- string) {
 }
 
 func main() {
+
+	//получаем n
 	fmt.Println("Введите число горутин-воркеров:")
 	var n int
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		fmt.Println(err)
 	}
+	//создаем буферизированные каналы ввода и вывода
 	jobs := make(chan int, 100)
 	results := make(chan string, 100)
 
+	//запускаем n воркеров
 	for i := 0; i < n; i++ {
 		go worker(i, jobs, results)
 	}
 
+	//записываем данные в канал
 	for i := 0; i < 2*n; i++ {
 		jobs <- i
 	}
-
+	close(jobs)
+	//выводим данные из канала
 	for i := 0; i < n; i++ {
 		s := <-results
 		fmt.Println(s)
