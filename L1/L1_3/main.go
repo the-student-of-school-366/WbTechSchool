0d2e676bb2e64b365c7b521a301bf1f91ebd8a0a
thestudentofschool@gmail.com <ananda.uldanov.2005@gmail.com>
2025-09-13 22:24:02 +0300

    Пофикшен баг

--- L1/L1_3/main.go
+++ L1/L1_3/main.go
@@ -2,45 +2,43 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"sync"
 	"time"
 )
 
-// worker принимает из канала ввода число, выводит его в консоль, имитирует работу, и вывод сообщение в канал вывода
-func worker(id int, jobs <-chan int, results chan<- string) {
+// worker принимает из канала ввода число, выводит его в консоль, имитирует работу
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for j := range jobs {
 		fmt.Println("worker", id, "выел:", j)
 		time.Sleep(time.Second)
-		results <- "Worker " + strconv.Itoa(id) + " закончил"
+
 	}
 }
 
 func main() {
 
-	//получаем n
+	wg := new(sync.WaitGroup)
+
 	fmt.Println("Введите число горутин-воркеров:")
 	var n int
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//создаем буферизированные каналы ввода и вывода
+	//создаем буферизированный канал
 	jobs := make(chan int, 100)
-	results := make(chan string, 100)
 
 	//запускаем n воркеров
 	for i := 0; i < n; i++ {
-		go worker(i, jobs, results)
+		wg.Add(1)
+		go worker(i, jobs, wg)
 	}
 
 	//записываем данные в канал
-	for i := 0; i < 2*n; i++ {
+	for i := 1; i <= n; i++ {
 		jobs <- i
 	}
 	close(jobs)
-	//выводим данные из канала
-	for i := 0; i < 2*n; i++ {
-		s := <-results
-		fmt.Println(s)
-	}
+	wg.Wait()
 }
