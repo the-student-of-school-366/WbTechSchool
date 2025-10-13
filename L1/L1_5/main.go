4037d15f7b7dcf35fcf36a5336ce977958d23328
thestudentofschool@gmail.com <ananda.uldanov.2005@gmail.com>
2025-10-13 13:13:37 +0300

    L1_5 сделана

--- L1/L1_5/main.go
+++ L1/L1_5/main.go
@@ -1,22 +1,44 @@
 package main
 
-//доделаю, простите, сейчас завал по учебе
 import (
+	"context"
 	"fmt"
-	"sync"
 	"time"
 )
 
-// worker принимает из канала ввода число, выводит его в консоль, имитирует работу
-func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for j := range jobs {
-		fmt.Println("worker", id, "выел:", j)
-		time.Sleep(time.Second)
+const N = 5
 
+// worker отправляет в канал числа, ждем сигнала завершения от Context
+func worker(ctx context.Context, dataChan chan<- int) {
+	defer close(dataChan)
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Получен сигнал завершения работы.")
+			return
+		case dataChan <- i:
+			fmt.Printf("Воркер отправил %d\n", i)
+			i++
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
 }
 
 func main() {
+	fmt.Printf("Программа будет работать %d секунд.\n\n", N)
 
+	// Создаем канал для обмена данными.
+	dataChan := make(chan int)
+
+	//Контекст, который автоматически завершится через 5 секунд
+	ctx, cancel := context.WithTimeout(context.Background(), N*time.Second)
+	defer cancel()
+
+	// Запускаем  горутину.
+	go worker(ctx, dataChan)
+
+	for value := range dataChan {
+		fmt.Printf("Читатель: получил %d\n", value)
+	}
 }
