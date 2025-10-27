a1cd7cd6701300896d9060cadc4e37de01425d38
thestudentofschool@gmail.com <ananda.uldanov.2005@gmail.com>
2025-10-27 11:17:32 +0300

    L1_8 сделана

--- L1/L1_8/main.go
+++ L1/L1_8/main.go
@@ -1,22 +1,32 @@
 package main
 
-//доделаю, простите, сейчас завал по учебе
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-// worker принимает из канала ввода число, выводит его в консоль, имитирует работу
-func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for j := range jobs {
-		fmt.Println("worker", id, "выел:", j)
-		time.Sleep(time.Second)
-
-	}
+func setBit(num int64, n int64) int64 {
+	return num | (1 << n)
 }
-
+func clearBit(num int64, n int64) int64 {
+	return num & ^(1 << n)
+}
 func main() {
-
+	fmt.Println("Введите число и номер бита")
+	var num int64
+	var n int64
+	_, err := fmt.Scan(&num, &n)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Если вы хотите установить бит в 1. введите 1, если в 0, введите 0")
+	var flag bool
+	_, err = fmt.Scan(&flag)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if flag {
+		ans := setBit(num, n)
+		fmt.Println(ans)
+	} else {
+		ans := clearBit(num, n)
+		fmt.Println(ans)
+	}
 }
